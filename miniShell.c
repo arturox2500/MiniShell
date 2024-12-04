9d24179ce9b138a00840996b351c35fd29bf6a76
IvanGutierrrez <146970754+IvanGutierrrez@users.noreply.github.com>
2024-12-04 10:40:51 +0100

    Update miniShell.c

--- miniShell.c
+++ miniShell.c
@@ -5,18 +5,29 @@
 #include <string.h>
 #include <sys/wait.h>
 #include "parser.h"
+#include <time.h>
 
-int ejecutar(int nc, tcommand * coms);//,char * in, char * out, char * err, int bg);
+int ejecutar(tline *line);
+void execute_cd(char *path);
+void print_dir();
+void redirect_stdin(char *input_file);
+void redirect_stdout(char *output_file);
+void redirect_stderr(char *error_file);
+pid_t hijosST[20] = {0};
 
 int main(int argc, char * argv[]) {
 	if (argc > 1){
 		printf("El ejecutable %s no necesita argumentos\n",argv[0]);
 		return 1;
 	}
+	time_t start = time(NULL);//hora inicial
 	char buf[1024];
-	tline * line;	
+	char *path, *aux;
+	int j;
+	tline * line;
 	while (1) {
-		printf("msh> ");
+		print_dir();
+		printf(" msh> ");
 		if (fgets(buf, 1024, stdin) == NULL){
 			if (feof(stdin)){
 				printf("\n");
@@ -26,17 +37,51 @@ int main(int argc, char * argv[]) {
 			continue;
 		}
 		line = tokenize(buf);
-		if (line==NULL) {
+		if (line == NULL) {
 			printf("Error al entender la linea\n");
 			continue;
 		}
-		ejecutar(line->ncommands,line->commands);// ,line->redirect_input,line->redirect_output,line->redirect_error,line->background);	
+		if (line->commands == NULL){
+			continue;
+		}
+		if(line->commands[0].filename != NULL){
+			ejecutar(line);	
+		} else {
+			buf[strcspn(buf, "\n")] = '\0';
+			aux = strtok(buf, " ");
+			if (aux != NULL) {
+				if (strcmp(aux, "cd") == 0){
+			    		path = strtok(NULL, " ");		    		
+			    		execute_cd(path);
+			    	} else if(strcmp(aux, "exit") == 0){
+			    		break;//Se cierra
+			    	} else {//no se encontro el mandato
+			    		continue;
+			    	}
+			}	
+		}
+		if (time(NULL) - start < 5){
+			for(j = 0; j < 20; j++){
+				if (hijosST[j] != 0){
+					pid_t result = waitpid(hijosST[j], NULL, WNOHANG);
+					if (result == hijosST[j]){
+						printf("El hijo con pid %d ha terminado\n",hijosST[j]);//BORRAR
+						hijosST[j] = 0;
+					}
+				}
+			}
+			start = time(NULL);
+		}
 	}
 	return 0;
 }
 
-int ejecutar(int nc, tcommand * coms){//,char * in, char * out, char * err, int bg){
-	int i,j;
+int ejecutar(tline *line){
+	int i,j,k;
+	int nc = line->ncommands;
+	tcommand * coms = line->commands;
+	FILE *file;
+	pid_t * hijosActual = (pid_t *)malloc(nc * sizeof(pid_t));
 	int ** pipes = (int **)malloc((nc - 1) * sizeof(int *));
 	for(j = 0; j < nc - 1; j++){
 		pipes[j] = (int *)malloc(2 * sizeof(int));
@@ -49,12 +94,21 @@ int ejecutar(int nc, tcommand * coms){//,char * in, char * out, char * err, int 
 			fprintf(stderr, "Falló el fort()\n%s\n", strerror(errno));
 			exit(-1);
 		} else if (pid == 0){ //Código hijo i
+			if (i == nc - 1 && line->redirect_output != NULL){
+				redirect_stdout(line->redirect_output);
+			}
+			if (i == nc - 1 && line->redirect_error != NULL){
+				redirect_stderr(line->redirect_error);
+			}
 			if (i == 0){ //1º Hijo a ejecutar
+				if (line->redirect_input != NULL){
+					redirect_stdin(line->redirect_input);
+				}
 				if (nc > 1){
 					dup2(pipes[i][1],STDOUT_FILENO);
 				}
 			} else if (i == nc - 1){//Último Hijo a ejecutar
-				dup2(pipes[i - 1][0],STDIN_FILENO);
+				dup2(pipes[i - 1][0],STDIN_FILENO);	
 			} else { //Los hijos del medio
 				dup2(pipes[i][1],STDOUT_FILENO);
 				dup2(pipes[i - 1][0],STDIN_FILENO);
@@ -69,6 +123,7 @@ int ejecutar(int nc, tcommand * coms){//,char * in, char * out, char * err, int 
 			perror("Error en execvp");
     			exit(1);
 		} else {
+			hijosActual[i] = pid;
 			if (i > 0){
 				close(pipes[i - 1][0]);
 				close(pipes[i - 1][1]);
@@ -79,8 +134,94 @@ int ejecutar(int nc, tcommand * coms){//,char * in, char * out, char * err, int 
 		free(pipes[j]);
 	}
 	free(pipes);
-	for (j = 0; j < nc; j++){
-		wait(NULL);
+	if (line->background == 0){//no es background
+		for (j = 0; j < nc; j++){
+			wait(NULL);
+		}
+	} else{//background
+		pid_t result;
+		k = 0;
+		for (j = 0; j < nc; j++){
+			result = waitpid(hijosActual[j], NULL, WNOHANG);
+			if (result == hijosActual[j]){
+				while (hijosST[k] != 0){
+					k++;
+				}
+				hijosST[k] = (pid_t)hijosActual[j];
+			}
+		}
 	}
+	free(hijosActual);
 	return 0;
+}
+
+void execute_cd(char *path){
+	char *home = getenv("HOME");
+	if (path == NULL){
+		path = home;
+	}	
+	if (chdir(path) != 0) {
+        	fprintf(stderr, "Fallo al cambiar la ruta\n%s\n", strerror(errno));
+    	}    	
+}
+
+void print_dir() {
+    char cwd[1024];
+    char *home = getenv("HOME");
+    
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        if (home != NULL && strncmp(cwd, home, strlen(home)) == 0) {
+            printf("~%s", cwd + strlen(home)); //reemplaza /home/user con ~ 
+        } else {
+            printf("%s\n", cwd);  // imprime directorio completo si no empieza en $HOME (/home/user)
+        }
+    } else {
+        fprintf(stderr, "Fallo al encontrar la ruta actual\n%s\n", strerror(errno));
+    }
+}
+
+void redirect_stdin(char *input_file) {
+	
+    	FILE *file = fopen(input_file, "r");  
+    	if (!file) {
+        	fprintf(stderr, "Falló lectura de fichero: %s\n", strerror(errno));
+        	return;
+    	}
+    // Redirigir STDIN a este fichero
+    	if (dup2(fileno(file), STDIN_FILENO) == -1) {
+        	fprintf(stderr, "Error al redirigir stdin: %s\n", strerror(errno));
+        	fclose(file);
+        	return;
+    	}
+    	fclose(file);  
+}
+
+void redirect_stdout(char *output_file) {
+    	FILE *file = fopen(output_file, "w"); 
+    	if (!file) {
+		fprintf(stderr, "Falló escritura a fichero %s: %s\n", output_file, strerror(errno));
+		return;
+    	}
+    // Redirigir STDOUT a este fichero
+    	if (dup2(fileno(file), STDOUT_FILENO) == -1) {
+		fprintf(stderr, "Error al redirigir stdout a %s: %s\n", output_file, strerror(errno));
+		fclose(file);
+		return;
+    	}
+    	fclose(file);  
+}
+
+void redirect_stderr(char *error_file) {
+    	FILE *file = fopen(error_file, "w");  
+    	if (!file) {
+        	fprintf(stderr, "Falló escritura a fichero: %s\n", strerror(errno));
+        	return;
+    	}
+    // Redirigir STDERR a este archivo
+    	if (dup2(fileno(file), STDERR_FILENO) == -1) {
+        	fprintf(stderr, "Error al redirigir stderr a %s: %s\n", error_file, strerror(errno));
+        	fclose(file);
+        	return;
+    }
+    	fclose(file);  
 }
